59bcf93059ad82ae5c713f337854ffce8def9914
RealtimeBagIdea <nonkung8899@hotmail.com>
2016-05-06 02:04:35 +0700

    Update connect.php

--- database/connect.php
+++ database/connect.php
@@ -2,7 +2,7 @@
 
 $mysql_host = 'localhost';
 $mysql_user = 'root';
-$mysql_pass = '02509371';
+$mysql_pass = 'xxxxxxxx';
 
 $database = new mysqli($mysql_host, $mysql_user, $mysql_pass, 'student');
 
